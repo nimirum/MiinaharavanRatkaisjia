86436efa856fbde91833205f28680d76f31b15de
nimirum <tunturikissa@gmail.com>
2015-03-13 19:13:08 +0200

    Projektin luominen
    JUnit, Cobertura ja PitTest

---
+++ MiinaharavanRatkaisija/src/main/java/nimirum/miinaharavanratkaisija/Main.java
@@ -0,0 +1,13 @@
+package nimirum.miinaharavanratkaisija;
+
+/**
+ *
+ * @author nimirum
+ */
+public class Main {
+    
+      public static void main(String[] args) {
+          System.out.println("Toimii");
+      }
+    
+}
