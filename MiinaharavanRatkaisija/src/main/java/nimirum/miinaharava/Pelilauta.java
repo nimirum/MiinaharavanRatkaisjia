d6cf7cf3daea23dce2744e201e5e073512f7feff
nimirum <tunturikissa@gmail.com>
2015-03-17 13:27:00 +0200

    Miinaharava lisätty

---
+++ MiinaharavanRatkaisija/src/main/java/nimirum/miinaharava/Pelilauta.java
@@ -0,0 +1,219 @@
+package nimirum.miinaharava;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Muodostaa Pelilaudan, joka muodostuu ruuduista(x,y), miinoittaa ja laskee
+ * numero arvot.
+ *
+ * @author nimirum
+ */
+public class Pelilauta {
+
+    private final int x;
+    private final int y;
+    private int miinojenMaara;
+    private final Ruutu pelilauta[][];
+    private int klikatutRuudut = 0;
+
+    /**
+     * Muodostaa pelilaudan johon kuuluu x*y määrä ruutuja
+     *
+     * @param x Pelilaudan leveys
+     * @param y Pelilaudan korkeus
+     */
+    public Pelilauta(int x, int y) {
+        this.x = x;
+        this.y = y;
+        pelilauta = new Ruutu[this.x][this.y];
+
+        for (int i = 0; i < this.x; i++) {
+            for (int j = 0; j < this.y; j++) {
+                pelilauta[i][j] = new Ruutu(i, j);
+            }
+        }
+        viereisetRuudut();
+    }
+
+    /**
+     *
+     * @param x Leveys koordinaatti
+     * @param y Korkeus koordinaatti
+     * @return Ruutu
+     */
+    public Ruutu getRuutu(int x, int y) {
+        if (0 <= x && x < this.x && 0 <= y && y < this.y) {
+            return pelilauta[x][y];
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @return Pelilauta kaksiulotteisena taulukkona
+     */
+    public Ruutu[][] getPelilauta() {
+        return pelilauta;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Asettaa pelilaudan ruudulle(x,y) miinan
+     *
+     * @param x leveys koordinaatti
+     * @param y korkeus koordinaatti
+     */
+    public void setMiina(int x, int y) {
+        Ruutu ruutu = getRuutu(x, y);
+        ruutu.setOnkoRuudussaMiina(true);
+    }
+
+    /**
+     * Lisää randomisti miinat ja lopuksi laskeNumerot()
+     *
+     * @param x
+     * @param y
+     */
+    public void miinoita(int x, int y) {
+        miinojenMaara = miinojenMaaraLaskuri();
+        Ruutu ekaKlikkaus = getRuutu(x, y);
+
+        Random random = new Random();
+        int min = 0;
+        int maxX = this.x - 1;
+        int maxY = this.y - 1;
+
+        int laskuri = 0;
+
+        while (laskuri < miinojenMaara) {
+            int randomNumX = random.nextInt((maxX - min) + 1) + min;
+            int randomNumY = random.nextInt((maxY - min) + 1) + min;
+            Ruutu ruutu = getRuutu(randomNumX, randomNumY);
+            if (!ruutu.getOnkoRuudussaMiina() && ruutu.getX() != ekaKlikkaus.getX() && ruutu.getY() != ekaKlikkaus.getY() && viereisetRuudutEiMiinoitetaTarkitus(ruutu.getX(), ruutu.getY(), ekaKlikkaus)) {
+                ruutu.setOnkoRuudussaMiina(true);
+                laskuri++;
+            }
+        }
+        laskeNumerot();
+    }
+
+    private boolean viereisetRuudutEiMiinoitetaTarkitus(int x, int y, Ruutu ruutu) {
+        for (Ruutu ruutuinen : ruutu.getViereisetRuudut()) {
+            if (ruutuinen.getX() == x && ruutuinen.getY() == y) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Laskee numero arvot miinojen viereen, eli kuinka moneen miinaan kyseinen
+     * ruutu koskee
+     */
+    public void laskeNumerot() {
+        for (int i = 0; i < this.x; i++) {
+            for (int j = 0; j < this.y; j++) {
+                Ruutu ruutu = getRuutu(i, j);
+                ruutu.laskeNumerot();
+            }
+        }
+    }
+
+    private int miinojenMaaraLaskuri() {
+        return (int) (0.15 * this.x * this.y);
+    }
+
+    public int getMiinojenMaara() {
+        return miinojenMaara;
+    }
+
+    private void viereisetRuudut() {
+        ArrayList<Ruutu> viereiset = new ArrayList<>();
+        for (int i = 0; i < this.x; i++) {
+            for (int j = 0; j < this.y; j++) {
+                Ruutu ruutu = getRuutu(i, j);
+                if (getRuutu(i + 1, j) != null) {
+                    viereiset.add(getRuutu(i + 1, j));
+                }
+                if (getRuutu(i + 1, j + 1) != null) {
+                    viereiset.add(getRuutu(i + 1, j + 1));
+                }
+                if (getRuutu(i, j + 1) != null) {
+                    viereiset.add(getRuutu(i, j + 1));
+                }
+                if (getRuutu(i, j - 1) != null) {
+                    viereiset.add(getRuutu(i, j - 1));
+                }
+                if (getRuutu(i + 1, j - 1) != null) {
+                    viereiset.add(getRuutu(i + 1, j - 1));
+                }
+                if (getRuutu(i - 1, j) != null) {
+                    viereiset.add(getRuutu(i - 1, j));
+                }
+                if (getRuutu(i - 1, j + 1) != null) {
+                    viereiset.add(getRuutu(i - 1, j + 1));
+                }
+                if (getRuutu(i - 1, j - 1) != null) {
+                    viereiset.add(getRuutu(i - 1, j - 1));
+                }
+                ruutu.setViereisetRuudut(viereiset);
+                viereiset = new ArrayList<>();
+            }
+        }
+    }
+
+    /**
+     * Avaa kaikki ruudut, eli vaihtaa ne näkyviksi
+     */
+    public void avaaKaikkiRuudut() {
+        for (int i = 0; i < this.x; i++) {
+            for (int j = 0; j < this.y; j++) {
+                Ruutu ruutu = getRuutu(i, j);
+                ruutu.setOnkoRuutuAvattu(true);
+            }
+        }
+    }
+
+    public int getKlikatutRuudut() {
+        return klikatutRuudut;
+    }
+
+    /**
+     * Kertoo piirtäjälle mikä on klikattujen eli avattujen ruutujen määrä
+     */
+    public void paivitaKlikatutRuudut() {
+        this.klikatutRuudut = 0;
+        for (int i = 0; i < this.x; i++) {
+            for (int j = 0; j < this.y; j++) {
+                Ruutu ruutu = getRuutu(i, j);
+                if (ruutu.getOnkoRuutuAvattu()) {
+                    klikatutRuudut++;
+                }
+            }
+        }
+    }
+
+    /**
+     * Laskee klikatuista ruuduista onko peli voitettu
+     *
+     * @return Onko klikattuja ruutuja riittävästi pelin voittamiseen
+     *
+     */
+    public boolean onkoPeliPaattynyt() {
+        paivitaKlikatutRuudut();
+        return (klikatutRuudut == (this.x * this.y - miinojenMaara));
+    }
+
+    @Override
+    public String toString() {
+        return getX() + "x" + getY();
+    }
+}
---
+++ MiinaharavanRatkaisija/src/main/java/nimirum/miinaharava/Ruutu.java
@@ -0,0 +1,162 @@
+package nimirum.miinaharava;
+
+import java.util.ArrayList;
+
+/**
+ * Pelilaudan ruutu, mikä voi olla miina, tyhjä tai numero, joka kertoo vieressä
+ * olevien miinojen määrän
+ *
+ * @author nimirum
+ */
+public class Ruutu {
+
+    private int viereistenMiinojenMaara = 0; //8 lähintä ruutua
+    private final int x;
+    private final int y;
+    private final int ruudunLeveys = 24;
+    private final int ruudunKorkeus = 24;
+    private ArrayList viereisetRuudut;
+    private boolean onkoMiina = false;
+    private boolean onkoRuutuAvattu = false;
+    private boolean klikattuMiina = false;
+    private boolean onkoLiputettu = false;
+
+    /**
+     *
+     * Parametrit x ja y ovat ruudun koordinaatit
+     *
+     * @param x Leveys koordinaatti
+     * @param y Korkeus koordinaatti
+     */
+    public Ruutu(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setViereisetRuudut(ArrayList<Ruutu> list) {
+        this.viereisetRuudut = list;
+    }
+
+    public ArrayList<Ruutu> getViereisetRuudut() {
+        return viereisetRuudut;
+    }
+
+    public void setOnkoRuudussaMiina(boolean onkoMiina) {
+        this.onkoMiina = onkoMiina;
+    }
+
+    public boolean getOnkoRuutuAvattu() {
+        return onkoRuutuAvattu;
+    }
+
+    /**
+     * Asettaa ruudnu näkyvyysarvon Piirtäjää varten False arvolla piirtää
+     * päällimmäisen ruudun True arvolla piirtää mitä ruutu on oikeasti
+     *
+     * @param onkoRuutuKlikattava
+     */
+    public void setOnkoRuutuAvattu(boolean onkoRuutuKlikattava) {
+        this.onkoRuutuAvattu = onkoRuutuKlikattava;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean getOnkoRuudussaMiina() {
+        return onkoMiina;
+    }
+
+    /**
+     * Käy ruudun viereiset ruudut läpi ja lisää viereisten miinojen määrää, jos
+     * itse ruudussa on miina
+     */
+    public void laskeNumerot() {
+        for (Ruutu viereinen : getViereisetRuudut()) {
+            if (viereinen.onkoMiina) {
+                addViereistenMiinojenMaaraa();
+            }
+        }
+    }
+
+    /**
+     * Ruudulle asetetaan viereisten miinojen määrä, arvo voi olla väliltä 0-8
+     *
+     * @param viereistenMiinojenMaara
+     */
+    public void setViereistenMiinojenMaara(int viereistenMiinojenMaara) {
+        if (0 <= viereistenMiinojenMaara && viereistenMiinojenMaara <= 8) {
+            this.viereistenMiinojenMaara = viereistenMiinojenMaara;
+        }
+    }
+
+    /**
+     * Lisää viereisten miinojen määrää yhdellä
+     */
+    public void addViereistenMiinojenMaaraa() {
+        viereistenMiinojenMaara++;
+    }
+
+    public int getViereistenMiinojenMaara() {
+        return viereistenMiinojenMaara;
+    }
+
+    public boolean isKlikattuMiina() {
+        return klikattuMiina;
+    }
+
+    /**
+     * Piirtäjää varten mahdollista asettaa mikä on klikattu miina
+     *
+     * @param klikattuMiina
+     */
+    public void setKlikattuMiina(boolean klikattuMiina) {
+        if (getOnkoRuudussaMiina()) {
+            this.klikattuMiina = klikattuMiina;
+        }
+    }
+
+    public int getRuudunKorkeus() {
+        return ruudunKorkeus;
+    }
+
+    public int getRuudunLeveys() {
+        return ruudunLeveys;
+    }
+
+    public boolean isOnkoRuutuLiputettu() {
+        return onkoLiputettu;
+    }
+
+    /**
+     * Ruudun voi liputtaa vain jos sitä ei ole klikattu vielä
+     *
+     * @param onkoLiputettu
+     */
+    public void setOnkoRuutuLiputettu(boolean onkoLiputettu) {
+        if (!getOnkoRuutuAvattu()) {
+            this.onkoLiputettu = onkoLiputettu;
+        }
+    }
+
+    /**
+     * Avaa kaikki tyhjän ruudun vieressä olevat tyhjät ruudut ja viereiset
+     * numeroruudut
+     */
+    public void avaaViereisetRuudut() {
+        for (Ruutu viereinen : getViereisetRuudut()) {
+            if (viereinen.onkoRuutuAvattu == false) {
+                if (viereinen.getViereistenMiinojenMaara() == 0) {
+                    viereinen.setOnkoRuutuAvattu(true);
+                    viereinen.avaaViereisetRuudut();
+                } else {
+                    viereinen.setOnkoRuutuAvattu(true);
+                }
+            }
+        }
+    }
+}
