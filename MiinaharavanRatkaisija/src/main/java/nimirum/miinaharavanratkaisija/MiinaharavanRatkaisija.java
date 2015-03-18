4eb53f38feae6514882e425b327195d602db4960
nimirum <tunturikissa@gmail.com>
2015-03-19 01:14:22 +0200

    MiinaharavanRatkaisija()

--- MiinaharavanRatkaisija/src/main/java/nimirum/miinaharavanratkaisija/MiinaharavanRatkaisija.java
+++ MiinaharavanRatkaisija/src/main/java/nimirum/miinaharavanratkaisija/MiinaharavanRatkaisija.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nimirum.miinaharavanratkaisija;
+
+import nimirum.miinaharava.Pelilauta;
 
 /**
  *
- * @author nexu770
+ * @author nimirum
  */
 public class MiinaharavanRatkaisija {
-    
+
+    private final Pelilauta lauta;
+    private boolean ruudut[][]; //tieto avatuista(true) ja suljetuista(false) ruuduista
+    private boolean miinat[][]; //tekoälyn laskemat paikat miinoille (true)=miina
+    private int viereistenMiinojenMaara[][]; //avattujen ruutujen viereisten miinojen määrä ruuduissa
+
+    public MiinaharavanRatkaisija() {
+        this(10, 10);
+    }
+
+    public MiinaharavanRatkaisija(int x, int y) {
+        lauta = new Pelilauta(x, y);
+        lauta.miinoita(x / 2, y / 2); // tekoölyn aloitusklikkaus aina keskelle
+        ruudut = new boolean[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                ruudut[i][j] = false;
+            }
+        }
+        miinat = new boolean[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                miinat[i][j] = false;
+            }
+        }
+    }
+
 }
